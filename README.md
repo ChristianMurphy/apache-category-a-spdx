9e6dda437595faa156fcc65873b79c6d596a758b
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-09-26 20:21:16 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # apache-category-a-spdx
-Apache Category A SPDX codes
+
+Apache Category A approved licenses in a machine readable format using [SPDX](https://spdx.org)
+
+## Installation
+
+```sh
+# NPM
+npm install apache-category-a-spdx
+
+# Yarn
+yarn add apache-category-a-spdx
+```
+
+## Usage
+
+```js
+var list = require("apache-category-a-spdx");
+
+for (var i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = [
+  "Apache-2.0",
+  "Apache-1.1",
+  "PHP-3.01",
+  // MX4J License
+  "BSD-2-Clause",
+  "BSD-3-Clause",
+  // DOM4J
+  "PostgreSQL",
+  "MIT",
+  "X11",
+  "ISC",
+  "SMLNJ",
+  // Cup Parser Generator
+  "ICU",
+  "NCSA",
+  "W3C",
+  // W3C Community Contributor License Agreement
+  "Xnet",
+  "Zlib",
+  "Libpng",
+  // FSF autoconf license
+  // DejaVu Fonts
+  "AFL-3.0",
+  // Service+Component+Architecture+Specifications
+  // OOXML XSD ECMA License
+  "MS-PL",
+  // Creative Commons Copyright-Only Dedication
+  "Python-2.0",
+  "APAFML",
+  "BSL-1.0",
+  // License for CERN packages in COLT
+  // UK Open Government License
+  "WTFPL",
+  // The Romantic WTF public license
+  "Unicode-DFS-2015",
+  "ZPL-2.0",
+  // ACE license
+  // Oracle Universal Permissive License (UPL) Version 1.0
+  "Open Grid Forum License"
+  // Google "Additional IP Rights Grant (Patents)" file
+];
